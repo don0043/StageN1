550209c641bfb34f3614e5dbd9ae519a4210a673
don0043 <daniiar.arkabaev@iaau.edu.kg>
2023-01-17 00:05:04 +0600

    finished dataTypes task

---
+++ dataTypes.js
@@ -0,0 +1,20 @@
+let name = "Daniiar";
+let age = 23;
+let bool = true;
+
+//addition of various types
+console.log(name + age)
+console.log(name + bool)
+console.log(age + bool)
+//multiplication of various types
+console.log(name * age)
+console.log(name * bool)
+console.log(age * bool)
+//Divide different types
+console.log(name / age)
+console.log(name / bool)
+console.log(age / bool)
+//explicit conversion
+console.log(String(age))
+console.log(Number(name))
+console.log(String(bool))//now its string "true"
