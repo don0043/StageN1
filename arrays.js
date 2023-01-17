3730c8f54f0b5e04613f71455a31003e46eb0099
don0043 <daniiar.arkabaev@iaau.edu.kg>
2023-01-18 04:04:36 +0600

    finished arrays task

---
+++ arrays.js
@@ -0,0 +1,107 @@
+arr = ["spider man", "iron man", "super man"]
+
+for (i = 0; i < arr.length; i++){
+    console.log(arr[i])
+}
+//array to string and back to array
+
+syms = ["s","u","p","e","r"]
+
+joined = syms.join("") //joining every symbol to string
+
+console.log(joined)
+
+
+splitted = joined.split("") //splitting it into array
+
+
+console.log(splitted)
+
+// hello to names
+
+names = ["Daniiar", "Sasha","Petya","Kolya"]
+
+helloNames = names.map((i) => {
+    return "hello" + " " + i
+})
+
+console.log(helloNames)
+
+
+//numeric to boolean
+
+
+numArr = [1, -1, 0, 3, 5]
+
+boolArr = numArr.map((e) => {
+    return Boolean(e)
+})
+
+
+console.log(boolArr)
+
+
+// sort array
+
+sArr = [1,6,7,8,3,4,5,6]
+
+
+console.log(sArr.sort().reverse())
+
+
+// filter by 3
+
+
+let n = sArr.filter((e) => {
+    return e === 3
+})
+
+console.log(n)
+
+// Write a function that takes two parameters - an array and a number and outputs the index of an array element equal to a number
+
+let func = (a, b) => {
+    console.log(a[b])
+}
+
+
+newArr = [1,3,4,5,67,8]
+
+func(newArr, 2)
+
+
+// Implement a loop that will print the number 'a' until it is less than 10
+
+// for (let i = 0; i < 10; i++){
+//     console.log(i)
+// }
+
+
+//prime numbers
+
+let primes = []
+
+for (let k = 0; k < 20; k++){
+    if ((k <= 1) || k % 2 === 0) {
+
+    } else {
+        primes.push(k)
+        ;
+    };
+
+}
+
+console.log(primes)
+
+//odd numbers
+
+
+let odds = []
+
+for (let k = 0; k < 60; k++){
+    if (k % 2 == 1) {
+    odds.push(k)
+    }
+}
+
+console.log(odds)
