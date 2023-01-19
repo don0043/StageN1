e6a2bcecf77c5066405649852e725820d63265d6
don0043 <daniiar.arkabaev@iaau.edu.kg>
2023-01-20 04:14:13 +0600

    finished date and strings task

---
+++ handling.js
@@ -0,0 +1,16 @@
+try {
+    console.log(a)
+
+    let a = 3;
+} catch {
+    console.log('let must be declared')
+}
+
+
+
+
+b = 2 / 0
+
+if (b == "Infinity"){
+    console.log("cannot be divided by zero")
+}
